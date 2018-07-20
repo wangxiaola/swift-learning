e17f330789ccbbe612e67c62c32b6d9324ed4a8a
wangxiaola <1015559188@qq.com>
2018-07-20 18:11:21 +0800

    swift4.0 学习实用

---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '8.0'
+
+target 'jrtt' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for jrtt
+
+pod 'Moya'
+pod 'Moya/RxSwift'
+pod 'RxSwift',    '~> 4.0'
+pod 'RxCocoa',    '~> 4.0'
+pod 'SnapKit',    '~> 4.0.0'
+pod 'Kingfisher'
+pod 'Then'
+pod 'RxDataSources'
+pod 'ObjectMapper'
+pod 'FLEX'
+pod 'MBProgressHUD'
+pod 'Toast'
+pod 'MJRefresh'
+pod 'SDCycleScrollView'
+
+end
---
+++ jrtt/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  jrtt
+//
+//  Created by 王小腊 on 2018/7/19.
+//  Copyright © 2018年 王小腊. All rights reserved.
+//
+
+// http://www.cocoachina.com/swift/20170911/20522.html
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+   // pod search RxSwift
+
+    // #MARK:- wee
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
